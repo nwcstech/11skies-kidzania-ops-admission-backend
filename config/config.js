4fde39018a10bcd0d0cb04f0b6282bb2f605e4ca
Dicky Chan <dickychan@nwcs.com.hk>
2024-07-26 14:25:30 +0800

    update db config

--- config/config.js
+++ config/config.js
@@ -1,33 +1,41 @@
 module.exports = {
-    development: {
-      username: process.env.POSTGRES_USER,
-      password: process.env.POSTGRES_PASSWORD,
-      database: process.env.POSTGRES_DB,
-      host: process.env.POSTGRES_HOST,
-      dialect: 'postgres',
-      dialectOptions: {
-        ssl: false
-      }
-    },
-    test: {
-      username: process.env.POSTGRES_USER,
-      password: process.env.POSTGRES_PASSWORD,
-      database: process.env.POSTGRES_DB,
-      host: process.env.POSTGRES_HOST,
-      dialect: 'postgres',
-      dialectOptions: {
-        ssl: false
-      }
-    },
-    production: {
-      username: process.env.POSTGRES_USER,
-      password: process.env.POSTGRES_PASSWORD,
-      database: process.env.POSTGRES_DB,
-      host: process.env.POSTGRES_HOST,
-      dialect: 'postgres',
-      dialectOptions: {
-        ssl: false
+  development: {
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+    host: process.env.POSTGRES_HOST,
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: false,
+        rejectUnauthorized: false
       }
     }
-  };
-  
+  },
+  test: {
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+    host: process.env.POSTGRES_HOST,
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: false,
+        rejectUnauthorized: false
+      }
+    }
+  },
+  production: {
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+    host: process.env.POSTGRES_HOST,
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: false,
+        rejectUnauthorized: false
+      }
+    }
+  }
+};
