6bb767a68869c579ceca1fedfef94b95bebfd3a9
Dicky Chan <dickychan@nwcs.com.hk>
2024-07-26 16:06:19 +0800

    update typo

--- models/index.js
+++ models/index.js
@@ -26,3 +26,5 @@ db.CheckIn.hasMany(db.GtsTicket, { foreignKey: 'check_in_id' });
 db.CheckIn.hasMany(db.Bracelet, { foreignKey: 'check_in_id' });
 db.GtsTicket.belongsTo(db.CheckIn, { foreignKey: 'check_in_id' });
 db.Bracelet.belongsTo(db.CheckIn, { foreignKey: 'check_in_id' });
+
+module.exports = db;
